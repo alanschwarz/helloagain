6132e28a564b182eb504c10c62655a5bcc59b42c
schwal <alan.schwarz@solvistas.com>
2021-10-07 13:24:10 +0200

    Linting

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,64 @@
 import React, { useState, useEffect } from "react";
-import { FlatList, SafeAreaView, ActivityIndicator, View, Text } from "react-native";
+import {
+  FlatList,
+  SafeAreaView,
+  ActivityIndicator,
+  View,
+  Text,
+} from "react-native";
 import styles from "./App.styles";
-import DataItem from './components/DataItem';
-import { useSelector } from 'react-redux';
-import { RootState } from './store/store';
-import fetchData from './services/bountieService';
+import DataItem from "./components/DataItem";
+import { useSelector } from "react-redux";
+import { RootState } from "./store/store";
+import fetchData from "./services/bountieService";
 
 const App = () => {
-    const [data, setData] = useState();
-    const [loading, setLoading] = useState(true);
+  const [data, setData] = useState();
+  const [loading, setLoading] = useState(true);
 
-    const collectedRewards = useSelector((state: RootState) => state.rewardsReducer.collectedRewards);
+  const collectedRewards = useSelector(
+    (state: RootState) => state.rewardsReducer.collectedRewards
+  );
 
-    useEffect(() => {
-        (async () => {
-            const data = await fetchData();
-            setData(data);
-            setLoading(false);
-        })();
-    }, []);
+  useEffect(() => {
+    (async () => {
+      const data = await fetchData();
+      if (data) {
+        setData(data);
+      }
+      setLoading(false);
+    })();
+  }, []);
 
-    const renderItem = ({ item }: any) => (
-        <DataItem title={item.name} points={item.needed_points} pictures={item.pictures} id={item.id} />
-    );
+  const renderItem = ({ item }: any) => (
+    <DataItem
+      title={item.name}
+      points={item.needed_points}
+      pictures={item.pictures}
+      id={item.id}
+    />
+  );
 
-    if (loading) {
-        return <ActivityIndicator />
-    }
+  if (loading) {
+    return <SafeAreaView style={styles.container}><ActivityIndicator /></SafeAreaView>;
+  }
 
-    return (
-        <>
-            <SafeAreaView style={styles.container}>
-                <FlatList
-                    data={data}
-                    renderItem={renderItem}
-                    keyExtractor={item => item.id}
-                    ListFooterComponent={<View style={{ height: 150 }} />}
-                />
-            </SafeAreaView>
-            <View style={styles.floatingBubble}>
-                <Text style={styles.floatingBubbleText}>{collectedRewards.length}</Text>
-                <Text style={styles.floatingBubbleLabel}>collected</Text>
-            </View>
-        </>
-    );
+  return (
+    <>
+      <SafeAreaView style={styles.container}>
+        <FlatList
+          data={data}
+          renderItem={renderItem}
+          keyExtractor={(item) => item.id}
+          ListFooterComponent={<View style={{ height: 150 }} />}
+        />
+      </SafeAreaView>
+      <View style={styles.floatingBubble}>
+        <Text style={styles.floatingBubbleText}>{collectedRewards.length}</Text>
+        <Text style={styles.floatingBubbleLabel}>collected</Text>
+      </View>
+    </>
+  );
 };
 
 export default App;
